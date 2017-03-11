a9a28c7b0511394f89d06f95628ee67f9219f7d1
Tsubasa Kato <tsubasa_kato@hotmail.com>
2017-03-11 20:00:16 +0900

    Shows how to save array with ArrayFire to text
    Might Need some Improvement.

---
+++ arrayfire_array_save.cpp
@@ -0,0 +1,62 @@
+/*******************************************************
+*(C) Tsubasa Kato 2017.3.11
+*Sample script for Saving Array as String passed from Arrayfire
+*Referenced: http://www.cplusplus.com/doc/tutorial/files/
+*            https://github.com/arrayfire/arrayfire/issues/230
+********************************************************/
+
+#include <arrayfire.h>
+#include <ios>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+using namespace af;
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	try {
+		//int device = argc > 1 ? atoi(argv[1]) : 0;
+		//af::setDevice(device);
+		//af::info();
+
+		af::array a = af::range(10);  // [0,  9]
+		a = a + 1;                    // [1, 10]
+		af_print(a);
+
+
+		std::ofstream myfile;
+		myfile.open("example2.txt");
+
+		af::array b = randu(3, f32);
+		float *host_b = new float[3];
+		b.host(host_b);
+
+
+		for (unsigned int a = 0; a < sizeof(host_b); a = a + 1)
+		{
+
+			myfile << host_b[a];
+			myfile << "\n";
+
+
+			printf("host_b = %f\n", host_b[a]);
+		}
+		myfile.close();
+		
+
+		delete[] host_b;
+
+
+
+
+	}
+	catch (af::exception& e) {
+
+		fprintf(stderr, "%s\n", e.what());
+		throw;
+	}
+
+	return 0;
+}
